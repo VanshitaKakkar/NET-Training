349416e3d2b123d25f4011c747c1d63001c4fdaf
vkakkar <vkakkar@covalience.com>
2026-01-09 19:12:03 +0530

    Scaffolding Assignment

---
+++ Scaffolding/Data/MVC_DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Scaffolding.Models;
+
+namespace Scaffolding.Data
+{
+    public class MVC_DbContext : DbContext
+    {
+        public MVC_DbContext (DbContextOptions<MVC_DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Scaffolding.Models.Category> Category { get; set; } = default!;
+    }
+}
